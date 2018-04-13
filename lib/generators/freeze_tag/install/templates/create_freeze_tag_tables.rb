64cf01cf3627a8a197a7ce8012912f32ac6e5790
Paul Franzen <paulfranzen@gmail.com>
2018-04-13 11:10:39 -0400

    add a tip for migrating past rails 4

--- lib/generators/freeze_tag/install/templates/create_freeze_tag_tables.rb
+++ lib/generators/freeze_tag/install/templates/create_freeze_tag_tables.rb
@@ -1,4 +1,12 @@
 class CreateFreezeTagTables < ActiveRecord::Migration
+
+  # TODO: You may experience the following error: 
+  # Directly inheriting from ActiveRecord::Migration is not supported. 
+  # Please specify the Rails release the migration was written for.
+  # If so, change the rails version to like: 
+  # class CreateFreezeTagTables < ActiveRecord::Migration[5.1]
+  # to your file
+
   def change
     
     raise "Please, make sure you've configured this correctly. Then remove this happy little warning :-)"
@@ -13,8 +21,8 @@ class CreateFreezeTagTables < ActiveRecord::Migration
       t.timestamps
     end
 
-    add_index :freeze_tags, [:taggable_type, :taggable_id, :tag, :expired_at, :list]
-    add_index :freeze_tags, [:taggable_type, :taggable_id, :expired_at, :list]
+    add_index :freeze_tags, [:taggable_type, :taggable_id, :tag, :expired_at, :list], name: "ft_tt_tid_t_exp_list"
+    add_index :freeze_tags, [:taggable_type, :taggable_id, :expired_at, :list], name: "ft_tt_tid_exp_list"
     add_index :freeze_tags, [:tag, :expired_at, :list]
     add_index :freeze_tags, [:taggable_type, :taggable_id, :tag, :list, :expired_at], unique: true, name: "fz_unique_tt_tid_t_li_ex_at"
   end
